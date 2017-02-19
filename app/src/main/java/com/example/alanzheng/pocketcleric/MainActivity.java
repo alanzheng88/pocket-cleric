a9a02ae7ab15f5b3958805f88f41e80ca9ab0eba
Alan Zheng <alanzheng@Alans-MacBook-Air.local>
2017-02-19 00:34:40 -0800

    add boilerplate files

---
+++ app/src/main/java/com/example/alanzheng/pocketcleric/MainActivity.java
@@ -0,0 +1,13 @@
+package com.example.alanzheng.pocketcleric;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}

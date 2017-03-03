8832e453749fef458abb3a7cd1266d1f8c198859
PrawnSkunk <mhcproske@gmail.com>
2017-03-02 22:12:02 -0800

    Added hotspot toggling

---
+++ app/src/main/java/com/example/alanzheng/pocketcleric/ApManager.java
@@ -0,0 +1,44 @@
+package com.example.alanzheng.pocketcleric;
+
+import android.content.*;
+import android.net.wifi.*;
+import android.util.Log;
+
+import java.lang.reflect.*;
+
+public class ApManager {
+
+    private static final String TAG = MainActivity.class.getSimpleName();
+
+    // Check if WiFi hotspot is enabled
+    public static boolean isApOn(Context context) {
+        WifiManager wifimanager = (WifiManager) context.getSystemService(context.WIFI_SERVICE);
+        try {
+            Method method = wifimanager.getClass().getDeclaredMethod("isWifiApEnabled");
+            method.setAccessible(true);
+            return (Boolean) method.invoke(wifimanager);
+        } catch (Throwable ignored) {
+            Log.d(TAG, "Could not invoke WiFiManager");
+        }
+        return false;
+    }
+
+    // Toggle WiFi hotspot
+    public static boolean configApState(Context context) {
+        WifiManager wifimanager = (WifiManager) context.getSystemService(context.WIFI_SERVICE);
+        WifiConfiguration wificonfiguration = null;
+        try {
+            // If WiFi is on, turn it off
+            if(isApOn(context)) {
+                wifimanager.setWifiEnabled(false);
+            }
+            Method method = wifimanager.getClass().getMethod("setWifiApEnabled", WifiConfiguration.class, boolean.class);
+            method.invoke(wifimanager, wificonfiguration, !isApOn(context));
+            return true;
+        }
+        catch (Exception e) {
+            e.printStackTrace();
+        }
+        return false;
+    }
+}
--- app/src/main/java/com/example/alanzheng/pocketcleric/MainActivity.java
+++ app/src/main/java/com/example/alanzheng/pocketcleric/MainActivity.java
@@ -1,13 +1,109 @@
 package com.example.alanzheng.pocketcleric;
 
-import android.support.v7.app.AppCompatActivity;
+import android.app.Activity;
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
+import android.provider.Settings;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends Activity implements View.OnClickListener {
 
-    @Override
+    private static final String TAG = MainActivity.class.getSimpleName();
+    private Button enableButton;
+    private Button disableButton;
+    private boolean systemWritePermission;
+    private boolean hotspotEnabled;
+
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // Find references to buttons
+        enableButton = (Button) findViewById(R.id.button_enable);
+        enableButton.setOnClickListener(this);
+
+        disableButton = (Button) findViewById(R.id.button_disable);
+        disableButton.setOnClickListener(this);
+    }
+
+    // Allow user to manually allow manifest to write permissions
+    private boolean checkSystemWritePermission() {
+
+        // Proceed if Android build is at least 6.0
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+
+            // Check if app can write system settings
+            boolean canWrite = Settings.System.canWrite(MainActivity.this);
+
+            // Open permissions menu if "Allow modify system settings" is disabled
+            if (canWrite) {
+                Log.d(TAG, "Device can already modify system settings.");
+                return true;
+            } else {
+                Log.d(TAG, "Insufficient permissions, launching settings.");
+                try {
+                    String packageName = getApplicationContext().getPackageName();
+                    Uri packageUri = Uri.parse("package:" + packageName);
+                    Intent settings = new Intent(Settings.ACTION_MANAGE_WRITE_SETTINGS, packageUri);
+                    startActivity(settings);
+                    Log.d(TAG, "Launched settings activity.");
+                } catch (Exception e) {
+                    Log.d(TAG, "Failed to launch Settings activity.");
+                }
+            }
+        }
+        return false;
+    }
+
+    // @+id/button_enable hook
+    public void onEnable(View v) {
+        if (!hotspotEnabled) {
+            // Handle system write permissions
+            if (systemWritePermission) {
+                // Check Ap state
+                if (!ApManager.isApOn(MainActivity.this)) {
+                    // Change Ap state
+                    ApManager.configApState(MainActivity.this);
+                    hotspotEnabled = true;
+                    Log.d(TAG, "WiFi hotspot enabled.");
+                } else {
+                    Log.d(TAG, "Could not enable WiFi hotspot.");
+                }
+            } else {
+                Log.d(TAG, "Checking system write permission.");
+                systemWritePermission = checkSystemWritePermission();
+            }
+        } else {
+            Log.d(TAG, "Hotspot already enabled.");
+        }
+    }
+
+    // @+id/button_disable hook
+    public void onDisable(View v) {
+        if (hotspotEnabled) {
+            // Check Ap state
+            if (ApManager.isApOn(MainActivity.this)) {
+                // Change Ap state
+                ApManager.configApState(MainActivity.this);
+                hotspotEnabled = false;
+                Log.d(TAG, "WiFi hotspot disabled.");
+            } else {
+                Log.d(TAG, "Could not disable WiFi hotspot.");
+            }
+        } else {
+            Log.d(TAG, "Hotspot already disabled.");
+        }
+    }
+
+    @Override
+    public void onClick(View v) {
+
     }
 }
+
+

f84e67c993c08a2e2f63280baac841f710a3054a
Alan Zheng <alanz@sfu.ca>
2017-03-05 12:44:17 -0800

    support jelly bean MR1, fix bug for wifi enabling and add more logs

--- app/src/main/java/com/example/alanzheng/pocketcleric/ApManager.java
+++ app/src/main/java/com/example/alanzheng/pocketcleric/ApManager.java
@@ -13,13 +13,16 @@ public class ApManager {
     // Check if WiFi hotspot is enabled
     public static boolean isApOn(Context context) {
         WifiManager wifimanager = (WifiManager) context.getSystemService(context.WIFI_SERVICE);
+        Method method = null;
         try {
-            Method method = wifimanager.getClass().getDeclaredMethod("isWifiApEnabled");
+            method = wifimanager.getClass().getDeclaredMethod("isWifiApEnabled");
             method.setAccessible(true);
+            Log.d(TAG, "Checking if Wifi AP is on");
             return (Boolean) method.invoke(wifimanager);
         } catch (Throwable ignored) {
             Log.d(TAG, "Could not invoke WiFiManager");
         }
+        Log.d(TAG, "isWifiApEnabled check was not performed. This line should not be reached.");
         return false;
     }
 
@@ -29,16 +32,23 @@ public class ApManager {
         WifiConfiguration wificonfiguration = null;
         try {
             // If WiFi is on, turn it off
-            if(isApOn(context)) {
+            if(wifimanager.isWifiEnabled()) {
+                Log.d(TAG, "Wifi is on. Turning Wifi Off");
                 wifimanager.setWifiEnabled(false);
             }
             Method method = wifimanager.getClass().getMethod("setWifiApEnabled", WifiConfiguration.class, boolean.class);
+            Log.d(TAG, "Ap is on before: " + isApOn(context));
             method.invoke(wifimanager, wificonfiguration, !isApOn(context));
+            Log.d(TAG, "sleeping for 5 seconds...");
+            Thread.sleep(5000);
+            Log.d(TAG, "finished sleeping");
+            Log.d(TAG, "Ap is on after: " + isApOn(context));
+            Log.d(TAG, "Configuring AP state succeeded. Wifi Ap is now " + (isApOn(context) ? "on" : "off") );
             return true;
-        }
-        catch (Exception e) {
+        } catch (Exception e) {
             e.printStackTrace();
         }
+        Log.d(TAG, "Configuring AP state failed");
         return false;
     }
 }

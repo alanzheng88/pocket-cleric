16c1019f1f36ade048848374bf5c249a8ecbe23e
Alan Zheng <alanz@sfu.ca>
2017-04-01 18:06:01 -0700

    finish recycler view

--- app/src/main/java/com/example/alanzheng/pocketcleric/MainActivity.java
+++ app/src/main/java/com/example/alanzheng/pocketcleric/MainActivity.java
@@ -21,7 +21,7 @@ public class MainActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
+            startActivity(new Intent(this, TethererActivity.class));
         // Clear shared login preferences
         // getSharedPreferences(PREFS_TAG, 0).edit().clear().commit();
     }
